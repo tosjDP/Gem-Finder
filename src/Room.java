1f8ef4c1a4e1284245843ec0f61a0c967c2335f2
marcdc-tm <marc.decaluwe@thomasmore.be>
2022-12-05 14:42:39 +0100

    attributen private + getExit

--- src/Room.java
+++ src/Room.java
@@ -15,11 +15,11 @@
 
 public class Room
 {
-    public String description;
-    public Room northExit;
-    public Room southExit;
-    public Room eastExit;
-    public Room westExit;
+    private String description;
+    private Room northExit;
+    private Room southExit;
+    private Room eastExit;
+    private Room westExit;
 
     /**
      * Create a room described "description". Initially, it has
@@ -50,6 +50,14 @@ public class Room
             southExit = south;
         if(west != null)
             westExit = west;
+    }
+
+    public Room getExit(String direction) {
+        if (direction.equals("north")) return northExit;
+        if (direction.equals("east")) return eastExit;
+        if (direction.equals("south")) return southExit;
+        if (direction.equals("west")) return westExit;
+        return null;
     }
 
     /**
