444f4564b3c0f456c997af1f1eb5118d936fc407
tosjDP <tosj@hotmail.be>
2022-12-29 21:10:27 +0100

    hp van player toegevoegd en win en lose

--- src/Player.java
+++ src/Player.java
@@ -8,26 +8,49 @@ public class Player {
     public static final int ITEM_GONE = 0;
     public static final int ITEM_NOTPRESENT = 1;
     public static final int ITEM_NOTMOVEABLE = 2;
+    public static final int ITEM_TO_HEAVY = 3;
+    public static final int DEAD = 0;
+    public static final int ALIVE = 1;
     private String name;
     private Room currentRoom;
     private Room previousRoom;
     private ArrayList<Item> bag = new ArrayList<>();
     private boolean canGoNextFloor = false;
     private Item gem;
+    private double maxWeight =20;
+    private int hp;
 
-    public Player(String name,Item gem) {
+    public Player(String name,int hp,Item gem,double maxWeight) {
         this.name = name;
         this.gem=gem;
+        this.maxWeight=maxWeight;
+        this.hp = hp;
+    }
+
+    public int getHp() {
+        return hp;
     }
 
     public int take(String name) {
+        double currentweight=0;
+        for (Item i : bag)
+            currentweight += i.getWeight();
         Item item = currentRoom.getItem(name);
+
+        if(currentweight+item.getWeight() > maxWeight) return ITEM_TO_HEAVY;
         if (item!=null && item.isMoveable()) {
             bag.add(item);
             return ITEM_GONE;
         }
         if (item==null) return ITEM_NOTPRESENT;
         return ITEM_NOTMOVEABLE;
+    }
+    public int takeDamage(){
+        if(hp >0) {
+            hp--;
+            return ALIVE;
+        }
+        return DEAD;
     }
 
     public String getName() {
@@ -50,8 +73,8 @@ public class Player {
         int gemCount = Collections.frequency(bag,gem);
         this.previousRoom = this.currentRoom;
         this.currentRoom = currentRoom;
-        if (this.currentRoom.type.equals(RoomType.FINISH)&&currentRoom.getFloor()==gemCount)
-         {
+
+        if (this.currentRoom.type.equals(RoomType.FINISH)&&currentRoom.getFloor()==gemCount) {
             this.canGoNextFloor=true;
         }
     }
